a6b2c0cfcc83ad12b89f7accc7583dfb0a9cf274
Bogdan-Kasyanchuk <bogdan.k1990@gmail.com>
2022-02-04 23:21:43 +0200

    delete coment

--- src/js/fullSizeImage.js
+++ src/js/fullSizeImage.js
@@ -3,5 +3,4 @@ import SimpleLightbox from 'simplelightbox';
 export let lightbox = new SimpleLightbox('.gallery__wrapper a', {
   captionsData: 'alt',
   captionDelay: '100',
-  // navText: ['<', '>'],
 });
